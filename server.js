a9accb1619d55d22dfa93de3583885d99a4e7e4b
aliescobarSCSU <escobara2@southernct.edu>
2025-09-20 03:43:02 +0000

    Added answers and modified server.js to serve files

--- server.js
+++ server.js
@@ -4,11 +4,23 @@ const url = require('url');
 //call back function
 serveStatic = function (req, res) {
 	let fileName = '.' + url.parse(req.url).pathname;
-	res.writeHead(200, {'Content-Type': 'text/plain' });
-	res.write("Place holder where the data should be sent");
+	fs.readFile(fileName, function(err, data) { 
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.write("404: File not found");
+	}else{
+	//detect file type
+		let contentType = 'text/plain';
+		if (fileName.endsWith('.jpg')) {
+			contentType = 'image/jpg';
+        	}
+		res.writeHead(200, {'Content-Type': contentType});
+	        res.write(data);
+	}
 	res.end();
+});
 }
- 
 const myserver = http.createServer(serveStatic); //create a server object
-myserver.listen(80, function() {console.log("Listening on port 80" )}); 
-
+myserver.listen(8080, function() {
+    console.log("Listening on port 8080");
+});
