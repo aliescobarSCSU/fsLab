e64d7e2c5d2c95e1df078c5f9deb1f7ab0313940
aliescobarSCSU <escobara2@southernct.edu>
2025-09-20 04:48:24 +0000

    Added answers to food for thought questions 6 and 7

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-//call back function
-serveStatic = function (req, res) {
-	let fileName = '.' + url.parse(req.url).pathname;
-	fs.readFile(fileName, function(err, data) { 
+
+// Callback function
+const serveStatic = function (req, res) {
+    let fileName = '.' + url.parse(req.url).pathname; 
+    fs.readFile(fileName, function(err, data) {
         if (err) {
+            // File not found
             res.writeHead(404, {'Content-Type': 'text/plain'});
             res.write("404: File not found");
-	}else{
-	//detect file type
-		let contentType = 'text/plain';
-		if (fileName.endsWith('.jpg')) {
-			contentType = 'image/jpg';
-        	}
-		res.writeHead(200, {'Content-Type': contentType});
-	        res.write(data);
-	}
-	res.end();
-});
+        } else {
+            // Detect file type
+            let contentType = 'text/plain';
+            if (fileName.endsWith('.jpg') || fileName.endsWith('.jpeg')) {
+                contentType = 'image/jpeg';
+            } else if (fileName.endsWith('.png')) {
+                contentType = 'image/png';
+            } else if (fileName.endsWith('.html')) {
+                contentType = 'text/html';
+            }
+
+            res.writeHead(200, {'Content-Type': contentType});
+            res.write(data); // send file data
+        }
+        res.end();
+    });
 }
+
 const myserver = http.createServer(serveStatic); //create a server object
 myserver.listen(8080, function() {
-    console.log("Listening on port 8080");
+    console.log("Server running: Listening on port 8080");
 });
